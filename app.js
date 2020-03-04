d53b7ee724c2391d0d42ea059c12c0d85ff085b4
Pale-Crusader <meticulous.planning@gmail.com>
2020-03-03 17:45:18 -0800

    fix console.log

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var userName = prompt('Welcome! What is your name?');
 alert(userName + ", it is nice to meet you! I am Matthew and I'd like you to know more about me.");
 
 var season = prompt(userName + ", please using a Y or N as an answer for yes or no: Do you think I was born in the winter?");
-console.log("Displaying if season letter is capital before toUpperCase: " + season);
+// console.log("Displaying if season letter is capital before toUpperCase: " + season);
 var season = season.toUpperCase();
-console.log("Displaying if season letter is capital after toUpperCase: " + season);
+// console.log("Displaying if season letter is capital after toUpperCase: " + season);
 if (season == 'Y') {
     alert("Sorry, " + userName + ", that is not correct, I was born on midsummer's eve.");
     }else if (season == 'N'){
@@ -18,9 +18,9 @@ if (season == 'Y') {
 
 
 var origin = prompt("Still with a Y or N as an answer for yes or no: " + userName + ", do you think I am from the Pacific Northwest?");
-console.log("Displaying if origin letter is capital before toUpperCase: " + origin);
+// console.log("Displaying if origin letter is capital before toUpperCase: " + origin);
 var origin = origin.toUpperCase();
-console.log("Displaying if origin letter is capital after toUpperCase: " + origin);
+// console.log("Displaying if origin letter is capital after toUpperCase: " + origin);
 if (origin == 'N') {
     alert("Sorry, " + userName + ", that is not correct. I was born at Fort Lewis and later served there");
     }else if (origin == 'Y'){
@@ -30,9 +30,9 @@ if (origin == 'N') {
     }
 
 var military = prompt("Still with a Y or N as an answer for yes or no: Do you think I am a veteran?");
-console.log("Displaying if military letter is capital before toUpperCase: " + military);
+// console.log("Displaying if military letter is capital before toUpperCase: " + military);
 var military = military.toUpperCase();
-console.log("Displaying if military letter is capital after toUpperCase: " + military);
+// console.log("Displaying if military letter is capital after toUpperCase: " + military);
 if (military == 'N') {
     alert(userName + ", I am sorry that is not correct. Is the page not loading?");
     }else if (military == 'Y'){
@@ -42,9 +42,9 @@ if (military == 'N') {
     }
 
 var goals = prompt("Still with a Y or N as an answer for yes or no: Do you think I want to be a coder?");
-console.log("Displaying if goal letter is capital before toUpperCase: " + goals);
+// console.log("Displaying if goal letter is capital before toUpperCase: " + goals);
 var goals = goals.toUpperCase();
-console.log("Displaying if goal letter is capital after toUpperCase: " + goals);
+// console.log("Displaying if goal letter is capital after toUpperCase: " + goals);
 if (goals == 'N') {
     alert(userName + ", that is not correct. I was born at Fort Lewis and later served there");
     }else if (goals == 'Y'){
@@ -54,9 +54,9 @@ if (goals == 'N') {
     }
 
 var eldest = prompt("Still with a Y or N as an answer for yes or no: Do you think ?");
-console.log("Displaying if goal letter is capital before toUpperCase: " + eldest);
+// console.log("Displaying if goal letter is capital before toUpperCase: " + eldest);
 var eldest = eldest.toUpperCase();
-console.log("Displaying if goal letter is capital after toUpperCase: " + eldest);
+// console.log("Displaying if goal letter is capital after toUpperCase: " + eldest);
 if (eldest == 'N') {
     alert("I am sorry that is not correct. I am the eldest son.");
     }else if (eldest == 'Y'){
